9a9caf1f2eabc72d895e14af1ee71984862e8ff9
Kevin Widjaja <kevin.widjaja21@gmail.com>
2023-03-23 21:54:54 +0800

    Revert "Revert copy script to old version"
    This reverts commit 7544e7ebd880454343b4df8f104ddcabb39f47e6.

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -9,31 +9,42 @@ rm -rvf ./hdw-su95x/out
 # copy from fbw source and su95x into one src
 cp -rva ./fbw-a32nx/src/. ./src
 cp -rva ./src/fbw_a320 ./src/fbw_su95x
-rm -rvf ./src/fbw_a320
 cp -rva ./src/fadec/a320_fadec ./src/fadec/su95_fadec
-rm -rvf ./src/fadec/a320_fadec
 
 cp -rva ./hdw-su95x/src/. ./src
 
 mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95
 mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95-lock-highlight
 
-cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui
+#make directories inside html_ui
+mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/SU95X_JS
+mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/SU95X_FONTS
+mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/SU95X_CSS
+mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/SU95X_Images
+mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/SU95X_Core
+mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/SU95X_Utils
+mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/SU95X
+mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/Airliners/Headwind_SU95X
+mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/SU95X_FlightElements
+mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/NavSystems/SU95X
+mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/SU95X_MAP
+mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VLivery/Liveries/SU95X_Registration
+mkdir -p ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VLivery/Liveries/SU95X_Printer
 
-# rename origin to fit for SU95X
-mv ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/JS ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/SU95X_JS
-mv ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Fonts ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/SU95X_FONTS
-mv ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/CSS ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/SU95X_CSS
-mv ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Images ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/SU95X_Images
-mv ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/A32NX_Core ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/SU95X_Core
-mv ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/A32NX_Utils ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/SU95X_Utils
-mv ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/A32NX ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/SU95X
-mv ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/Airliners/Headwind_SU95X
-mv ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/FlightElements ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/SU95X_FlightElements
-mv ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/NavSystems/A320_Neo ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/NavSystems/SU95X
-mv ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/MAP ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/SU95X_MAP
-mv ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VLivery/Liveries/A32NX_Registration ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VLivery/Liveries/SU95X_Registration
-mv ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VLivery/Liveries/Printer ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VLivery/Liveries/SU95X_Printer
+# copy html_ui for SU95X
+cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/JS/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/SU95X_JS
+cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/Fonts/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/SU95X_FONTS
+cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/CSS/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/SU95X_CSS
+cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/Images/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/SU95X_Images
+cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/SU95X_Core
+cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Utils/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/SU95X_Utils
+cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/A32NX/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/SU95X
+cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/Airliners/Headwind_SU95X
+cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/FlightElements/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/SU95X_FlightElements
+cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/NavSystems/A320_Neo/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/NavSystems/SU95X
+cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/MAP/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VCockpit/Instruments/SU95X_MAP
+cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/Pages/VLivery/Liveries/A32NX_Registration/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VLivery/Liveries/SU95X_Registration
+cp -rva ./fbw-a32nx/flybywire-aircraft-a320-neo/html_ui/Pages/VLivery/Liveries/Printer/. ./hdw-su95x/out/headwindsim-aircraft-su100-95/html_ui/Pages/VLivery/Liveries/SU95X_Printer
 
 # copy base of SU95X to out
 cp -rva ./hdw-su95x/src/base/headwindsim-aircraft-su100-95/. ./hdw-su95x/out/headwindsim-aircraft-su100-95
