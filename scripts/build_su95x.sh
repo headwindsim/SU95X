d68218656f7a096bf1c1d72a41bce3b1bf8c117d
Kevin Widjaja <kevin.widjaja21@gmail.com>
2023-05-13 23:04:37 +0800

    chore: Added the --no-cache parameter to the build scripts
    also fix: remove instrument did not load message

--- scripts/build_su95x.sh
+++ scripts/build_su95x.sh
@@ -11,6 +11,14 @@ if [ "${GITHUB_ACTIONS}" == "true" ]; then
   chown -R root:root /external
 fi
 
+# Loop through the arguments
+for arg in "$@"; do
+  if [ "$arg" == "--no-cache" ]; then
+    echo "Removing out directory /external/build-su95x/out"
+    rm -rf /external/build-su95x/out
+  fi
+done
+
 # run build
 time npx igniter -r su95x "$@"
 
