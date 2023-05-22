17e03802f34a60a02359ce566aa7c1b8655f6a85
Kevin Widjaja <kevin.widjaja21@gmail.com>
2023-05-22 21:49:24 +0800

    payload manager re-write part 1

--- hdw-su95x/src/base/headwindsim-aircraft-su100-95/html_ui/Pages/SU95X_Core/A32NX_PayloadManager.js
+++ hdw-su95x/src/base/headwindsim-aircraft-su100-95/html_ui/Pages/SU95X_Core/A32NX_PayloadManager.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 class A32NX_PayloadConstructor {
     constructor() {
         this.paxStations = {
@@ -5,56 +6,41 @@ class A32NX_PayloadConstructor {
                 name: 'ROWS [1-4]',
                 seats: 18,
                 weight: Math.round(NXUnits.kgToUser(1440)),
-                pax: 0,
-                paxTarget: 0,
                 stationIndex: 0 + 1,
                 position: 15.8,
-                seatsRange: [1, 18],
-                simVar: "SU95_PAX_TOTAL_ROWS_1_4"
+                simVar: "A32NX_PAX_A"
             },
             rows5_8: {
                 name: 'ROWS [5-8]',
                 seats: 20,
                 weight: Math.round(NXUnits.kgToUser(1600)),
-                pax: 0,
-                paxTarget: 0,
                 stationIndex: 1 + 1,
                 position: 5.0,
-                seatsRange: [19, 38],
-                simVar: "SU95_PAX_TOTAL_ROWS_5_8"
+                simVar: "A32NX_PAX_B"
             },
             rows9_12: {
                 name: 'ROWS [9-12]',
                 seats: 20,
                 weight: Math.round(NXUnits.kgToUser(1600)),
-                pax: 0,
-                paxTarget: 0,
                 stationIndex: 2 + 1,
                 position: -6.4,
-                seatsRange: [39, 58],
-                simVar: "SU95_PAX_TOTAL_ROWS_9_12"
+                simVar: "A32NX_PAX_C"
             },
             rows13_16: {
                 name: 'ROWS [13-16]',
                 seats: 20,
                 weight: Math.round(NXUnits.kgToUser(1600)),
-                pax: 0,
-                paxTarget: 0,
                 stationIndex: 3 + 1,
                 position: -16.6,
-                seatsRange: [59, 78],
-                simVar: "SU95_PAX_TOTAL_ROWS_13_16"
+                simVar: "A32NX_PAX_D"
             },
             rows17_20: {
                 name: 'ROWS [17-20]',
                 seats: 20,
                 weight: Math.round(NXUnits.kgToUser(1600)),
-                pax: 0,
-                paxTarget: 0,
                 stationIndex: 4 + 1,
                 position: -27.5,
-                seatsRange: [79, 98],
-                simVar: "SU95_PAX_TOTAL_ROWS_17_20"
+                simVar: "A32NX_PAX_E"
             },
         };
 
@@ -66,7 +52,7 @@ class A32NX_PayloadConstructor {
                 stationIndex: 5 + 1,
                 position: 12.6,
                 visible: true,
-                simVar: 'SU95_CARGO_FWD_BAGGAGE',
+                simVar: 'A32NX_CARGO_FWD_BAGGAGE',
             },
             aftCont: {
                 name: 'AFT BAGGAGE 1',
@@ -75,7 +61,7 @@ class A32NX_PayloadConstructor {
                 stationIndex: 6 + 1,
                 position: -16.2,
                 visible: true,
-                simVar: 'SU95_CARGO_AFT_BAGGAGE_1',
+                simVar: 'A32NX_CARGO_AFT_BAGGAGE_1',
             },
             aftBag: {
                 name: 'AFT BAGGAGE 2',
@@ -84,7 +70,7 @@ class A32NX_PayloadConstructor {
                 stationIndex: 7 + 1,
                 position: -23.8,
                 visible: true,
-                simVar: 'SU95_CARGO_AFT_BAGGAGE_2',
+                simVar: 'A32NX_CARGO_AFT_BAGGAGE_2',
             },
             aftBulk: {
                 name: 'AFT BULK/LOOSE',
@@ -93,7 +79,7 @@ class A32NX_PayloadConstructor {
                 stationIndex: 8 + 1,
                 position: -30.6,
                 visible: true,
-                simVar: 'SU95_CARGO_AFT_BULK_LOOSE',
+                simVar: 'A32NX_CARGO_AFT_BULK_LOOSE',
             },
         };
     }
@@ -105,8 +91,8 @@ const cargoStations = payloadConstruct.cargoStations;
 const MAX_SEAT_AVAILABLE = 98;
 
 /**
-     * Calculate %MAC ZWFCG of all stations
-     */
+ * Calculate %MAC ZWFCG of all stations
+ */
 function getZfwcg() {
 
     const leMacZ = -3.900; // Accurate to 3 decimals, replaces debug weight values
@@ -117,8 +103,8 @@ function getZfwcg() {
     const emptyMoment = emptyPosition * emptyWeight;
     const PAX_WEIGHT = SimVar.GetSimVarValue("L:A32NX_WB_PER_PAX_WEIGHT", "Number");
 
-    const paxTotalMass = Object.values(paxStations).map((station) => (SimVar.GetSimVarValue(`L:${station.simVar}`, "Number") * PAX_WEIGHT)).reduce((acc, cur) => acc + cur, 0);
-    const paxTotalMoment = Object.values(paxStations).map((station) => (SimVar.GetSimVarValue(`L:${station.simVar}`, "Number") * PAX_WEIGHT) * station.position).reduce((acc, cur) => acc + cur, 0);
+    const paxTotalMass = Object.values(paxStations).map((station) => new BitFlags(SimVar.GetSimVarValue(`L:${station.simVar}`, "Number")).getTotalBits() * PAX_WEIGHT).reduce((acc, cur) => acc + cur, 0);
+    const paxTotalMoment = Object.values(paxStations).map((station) => new BitFlags(SimVar.GetSimVarValue(`L:${station.simVar}`, "Number")).getTotalBits() * PAX_WEIGHT * station.position).reduce((acc, cur) => acc + cur, 0);
 
     const cargoTotalMass = Object.values(cargoStations).map((station) => SimVar.GetSimVarValue(`PAYLOAD STATION WEIGHT:${station.stationIndex}`, getUserUnit())).reduce((acc, cur) => acc + cur, 0);
     const cargoTotalMoment = Object.values(cargoStations).map((station) => (SimVar.GetSimVarValue(`PAYLOAD STATION WEIGHT:${station.stationIndex}`, getUserUnit()) * station.position)).reduce((acc, cur) => acc + cur, 0);
